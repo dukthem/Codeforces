d52a69a1d30211c20122bfe35ad7cd52cfd95faf
Dukthem <dukthem@gmail.com>
2024-09-09 11:08:25 +0530

    Practcice

--- CF1A.py
+++ CF1A.py
@@ -1,4 +1,16 @@
 import math
 n , m , a = list(map(int, input().split()))
-# tiles = (n*m) / (a*a)
-# print(math.ceil(tiles))
+tiles = (n*m) // (a*a)
+ar_fl = n*m
+ar_ti = a*a
+if n <= a and m <= a:
+    print(1)
+elif n > a and m <= a:
+    print(math.ceil(n/a))
+elif n < a and m >= a:
+    print(math.ceil(m/a))
+else:
+    if ar_fl % ar_ti == 0 and n%a ==0 and m % a== 0:
+        print(tiles)
+    else:
+        print(math.ceil((n/a))*math.ceil(m/a))

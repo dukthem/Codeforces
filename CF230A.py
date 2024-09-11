f330361715b3baaf352b0e27016fe37d7994a35a
Dukthem <dukthem@gmail.com>
2024-09-11 10:28:27 +0530

    Practice

--- CF230A.py
+++ CF230A.py
@@ -1,1 +1,16 @@
 s, n = list(map(int, input().split()))
+y = []
+cnt = 0
+for i in range(n):
+    xy = list(map(int, input().split()))
+    y.append(xy)
+y.sort()
+for i in range(len(y)):
+    if y[i][0] >= s:
+        print("NO")
+        cnt += 1
+        break
+    else:
+        s += y[i][1]
+if cnt == 0:
+    print("YES")

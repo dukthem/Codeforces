7b1cfcdcd04e2425da8d361cffda2a894fc16c84
Dukthem <dukthem@gmail.com>
2024-09-15 18:29:36 +0530

    Practice

---
+++ CF1370A.py
@@ -0,0 +1,5 @@
+import math
+t = int(input())
+for i in range(t):
+    c = int(input())
+    print(c//2)
---
+++ CF2005B1.py
@@ -0,0 +1,15 @@
+t = int(input())
+for i in range(t):
+    n, m, q = list(map(int, input().split()))
+    b = sorted(list(map(int, input().split())))
+    a = int(input())
+    if a < b[0]:
+        print(b[0]-1)
+    elif a > b[1]:
+        print(n-b[1])
+    else:
+        s = b[1] - b[0] - 1
+        if s % 2 == 0:
+            print(s//2)
+        else:
+            print((s+1)// 2)

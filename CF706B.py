5e4ca105b717eb8dc4825b7371f34b98f8556b25
Daman Thakur <damanthakur@Damans-MacBook-Air.local>
2024-09-20 05:29:52 +0530

    Practice

--- CF706B.py
+++ CF706B.py
@@ -1,7 +1,20 @@
+def find_between(lst, num):
+    low, high = 0, len(lst)-1
+    if num < lst[low]  or num > lst[high]:
+        return None
+    while low <= high:
+        mid = (low + high) // 2
+        if lst[mid] == num:
+            return mid, mid
+        elif lst[mid] < num:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return high, low
+    
 n = int(input())
 x = list(map(int, input().split()))
 x.sort()
-# print(x)
 q = int(input())
 for i in range(q):
     m = int(input())
@@ -10,23 +23,34 @@ for i in range(q):
     elif m >= x[len(x)-1]:
         print(n)
     else:
-        ans = 0
-        s = 0
-        l = n
-        cnt = 0
-        while ans == 0:   
-            for j in range(s, l):
-                mid = (l+s) // 2
-                if x[mid] > m:
-                    l = mid
-                    break 
-                else:
-                    cnt += (l+s)// 2
-                    s = mid
-                    break
-            # for j in range(s, l):
-            #     if x[j] <= m:
-            #         cnt += 1
-            # print(cnt)
-            ans = 1
+        result = find_between(x, m)
+        cnt = result[0]+1
+        
+        if x[result[0] + 1] <= m:
+            while x[result[0]+1] <= m:
+                cnt += 1
+            print(cnt)
+        else:
+            print(cnt)
+        
+        
+        # ans = 0
+        # s = 0
+        # l = n
+        # cnt = 0
+        # while ans == 0:   
+        #     for j in range(s, l):
+        #         mid = (l+s) // 2
+        #         if x[mid] > m:
+        #             l = mid
+        #             break 
+        #         else:
+        #             cnt += (l+s)// 2
+        #             s = mid
+        #             break
+        #     # for j in range(s, l):
+        #     #     if x[j] <= m:
+        #     #         cnt += 1
+        #     # print(cnt)
+        #     ans = 1
             
